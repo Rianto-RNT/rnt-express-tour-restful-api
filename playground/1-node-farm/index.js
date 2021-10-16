764f024baa2dd796ebdca73b1e26df2521ed9cfa
Rianto <rianto.rnt@gmail.com>
2021-10-16 13:14:26 +0800

    routing

--- playground/1-node-farm/index.js
+++ playground/1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-const http = require('http');
+const http = require("http");
+const url = require("url");
 
 //======================================
 // FILE
@@ -16,7 +17,6 @@ const http = require('http');
 // fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 //     if (err) return console.log('ERROR! 💥');
 
-
 //   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
 //     console.log(data2);
 //     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
@@ -32,12 +32,22 @@ const http = require('http');
 //============================================================
 
 // Server
-const server = http.createServer((req, res)=> {
-    console.log(req);
-    res.end('Hello server here...');
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+      res.writeHead(404, {
+         'Content-type': 'text/html',
+         'my-own-header': 'hello-world' 
+      });
+      res.end('<h1>Page not found</h1>');
+  }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
-
