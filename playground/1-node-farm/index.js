f34de9b944ccf48b7473a2bdf6de0027e709d95d
Rianto <rianto.rnt@gmail.com>
2021-10-16 11:39:44 +0800

    Reading and Writing File Asynchronously

--- playground/1-node-farm/index.js
+++ playground/1-node-farm/index.js
@@ -1,8 +1,27 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// Blocking, Synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about the avovcado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!')
+// const textOut = `This is what we know about the avovcado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// Non-blocking, Asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    if (err) return console.log('ERROR! 💥');
+
+
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written 😁");
+      });
+    });
+  });
+});
+console.log("will read file!");
