02ac8d5a59c7250471af94f4b7530d94dc24cde8
Rianto <rianto.rnt@gmail.com>
2021-10-16 12:27:28 +0800

    Creating a simple web server

--- playground/1-node-farm/index.js
+++ playground/1-node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require('http');
+
+//======================================
+// FILE
 
 // Blocking, Synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -9,19 +13,31 @@ const fs = require("fs");
 // console.log("File written!");
 
 // Non-blocking, Asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-    if (err) return console.log('ERROR! 💥');
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//     if (err) return console.log('ERROR! 💥');
 
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written 😁");
-      });
-    });
-  });
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written 😁");
+//       });
+//     });
+//   });
+// });
+// console.log("will read file!");
+//============================================================
+
+// Server
+const server = http.createServer((req, res)=> {
+    console.log(req);
+    res.end('Hello server here...');
 });
-console.log("will read file!");
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
+
