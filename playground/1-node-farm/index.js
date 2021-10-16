85316e21de7d6c40fd3b92d9f2a051d5f20eff10
Rianto <rianto.rnt@gmail.com>
2021-10-16 13:50:43 +0800

    new data

--- playground/1-node-farm/index.js
+++ playground/1-node-farm/index.js
@@ -35,16 +35,23 @@ const url = require("url");
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-  if (pathName === '/' || pathName === "/overview") {
+  if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    fs.readFile(`${__dirname}./dev-data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      console.log(productData);
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+    });
   } else {
-      res.writeHead(404, {
-         'Content-type': 'text/html',
-         'my-own-header': 'hello-world' 
-      });
-      res.end('<h1>Page not found</h1>');
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
   }
 });
 
