7ddc59d14f48bde1f5b1d5bcf32ec6933c49cd0b
Rianto <rianto.rnt@gmail.com>
2021-10-21 18:36:48 +0800

    setting up express and basic routing

---
+++ playground/4-natours/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello server nih', app: 'RNT Creative' });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
