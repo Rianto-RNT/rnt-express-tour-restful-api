648ae53eb51abd98c6b2136bfcfd93cf71a2da1e
Rianto <rianto.rnt@gmail.com>
2021-10-22 07:49:41 +0800

    responding to URL parameters

--- playground/4-natours/app.js
+++ playground/4-natours/app.js
@@ -24,6 +24,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  
+//   if(id > tours.length) {
+  if(!tour) {
+  return res.status(404).json({
+          status: 'Failed',
+          message: 'Invalid ID'
+      })
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
@@ -34,7 +55,7 @@ app.post('/api/v1/tours', (req, res) => {
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    err => {
+    (err) => {
       res.status(201).json({
         status: 'success',
         data: {
@@ -43,7 +64,6 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-
 });
 
 const port = 3000;
